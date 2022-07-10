d3669fc7664f159795ec10112786c0f93538c65d
Fidel Leon (HP) <fidel@flm.cat>
2022-07-10 18:08:11 +0200

    End of 3.12 - Passing data via props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,13 @@
 import "./ExpenseItem.css";
 
-const ExpenseItem = () => {
+const ExpenseItem = (props) => {
+
     return (
         <div className="expense-item">
-            <div>March 8th, 2021</div>
+            <div>{props.date.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price">€294.67</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">{props.amount}</div>
             </div>
         </div>
     );
